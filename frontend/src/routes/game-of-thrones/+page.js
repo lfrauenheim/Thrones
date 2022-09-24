d6c99de0770b4e457e88b74b0ca52e83586e0174
Lenard Frauenheim <81096210+lfrauenheim@users.noreply.github.com>
2022-09-24 19:49:36 +0200

    Update +page.js

--- frontend/src/routes/game-of-thrones/+page.js
+++ frontend/src/routes/game-of-thrones/+page.js
@@ -1,7 +1,7 @@
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ fetch }) {
-  const res = await fetch('https://thrones-api.firefarm.netapi/got/characters')
+  const res = await fetch('https://thrones-api.firefarm.net/api/got/characters')
 
   return {
     characters: await res.json()
